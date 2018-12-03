ed57cae322e0bc5d2170cdd5ac5f14140c02e492
Mahogany Lambert <mnlamber@asu.edu>
2018-12-03 11:51:00 -0700

    Hello World
    This is a 'Hello, World!' exercise in Java.

---
+++ hello.java
@@ -0,0 +1,15 @@
+/*
+Name: Introduction to Variables
+Date: December 3, 2018
+Description: This is program that helps me practice creating variables. It is a remake of CSE 110 Lab 1. This is apart
+of my December challenge to revamp my college projects and post them to GitHub.
+Author: Mahogany Lambert
+ */
+
+//This class prints out the oh so famous 'Hello, World!' to the console.
+public class hello {
+     public static void main (String[] args) {
+         System.out.println("Hello, World!");
+     }
+
+}
